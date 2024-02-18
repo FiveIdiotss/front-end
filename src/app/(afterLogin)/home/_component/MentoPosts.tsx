93f8da8107c9709ccbd9a735e4be33eff5b8941e
minki1220 <130380152+minki1220@users.noreply.github.com>
2024-02-18 18:55:22 +0900

    멘토 그리드

--- src/app/(afterLogin)/home/_component/MentoPosts.tsx
+++ src/app/(afterLogin)/home/_component/MentoPosts.tsx
@@ -6,6 +6,7 @@ import MentoPostCard from '../../_component/MentoPostCard';
 import Link from 'next/link';
 import Image from 'next/image';
 import nextImage from '@/../public/next.svg';
+import { useRouter } from 'next/navigation';
 const mentorPosts = [
     'title 1',
     'title 2',
@@ -26,6 +27,7 @@ const mentorPosts = [
 ]; //하드코딩 된 멘토 글
 
 function MentoPosts() {
+    const router = useRouter();
     const [itemsPerSlide, setItemsPerSlide] = useState<number>(100);
     const isSmallScreen = useMediaQuery({ query: '(max-width: 600px)' });
     const isMediumScreen = useMediaQuery({ query: '(min-width: 601px) and (max-width: 1300px)' });
@@ -43,33 +45,35 @@ function MentoPosts() {
         mentorPosts.slice(i * itemsPerSlide, i * itemsPerSlide + itemsPerSlide),
     );
     return (
-        <div className="flex w-full flex-col ">
-            <Link className="mb-3 flex w-fit flex-row pl-6 pt-12 " href="/mentors">
-                <span className="text-2xl font-semibold">멘토들의 지식 공유</span>
-                <Image src={nextImage} alt="mentee" />
-            </Link>
-            {itemsPerSlide !== 100 && (
-                <MainCarousel itemsPerSlide={itemsPerSlide}>
-                    {chunkedPosts
-                        .filter((chunk) => chunk.length === itemsPerSlide)
-                        .map((chunk, i) => (
-                            <div key={i} className="flex w-full justify-center   ">
-                                <div className="flex w-[calc(100%-48px)] flex-row gap-3">
-                                    {chunk.map((post, index) => (
-                                        <div
-                                            key={index}
-                                            className="my-2 flex  flex-1 rounded-lg border border-neutral-100 text-center shadow "
-                                        >
-                                            <MentoPostCard post={post} />
-                                        </div>
-                                    ))}
+        <>
+            <div className="flex w-full flex-col ">
+                <Link className="mb-3 flex w-fit flex-row pl-6 pt-12 " href="/home">
+                    <span className="text-2xl font-semibold">멘토들의 지식 공유</span>
+                    <Image src={nextImage} alt="mentee" />
+                </Link>
+                {itemsPerSlide !== 100 && (
+                    <MainCarousel itemsPerSlide={itemsPerSlide}>
+                        {chunkedPosts
+                            .filter((chunk) => chunk.length === itemsPerSlide)
+                            .map((chunk, i) => (
+                                <div key={i} className="flex w-full justify-center   ">
+                                    <div className="flex w-[calc(100%-48px)] flex-row gap-3">
+                                        {chunk.map((post, index) => (
+                                            <div
+                                                key={index}
+                                                className="my-2 flex  flex-1 rounded-lg border border-neutral-100 text-center shadow"
+                                                onClick={() => router.push(`home/mento_Id/${index}`)}
+                                            >
+                                                <MentoPostCard post={post} />
+                                            </div>
+                                        ))}
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
-                </MainCarousel>
-            )}
-        </div>
+                            ))}
+                    </MainCarousel>
+                )}
+            </div>
+        </>
     );
 }
-
 export default MentoPosts;
