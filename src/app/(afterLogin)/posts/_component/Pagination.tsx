eb7f70f3792420e0cdbde18f892e837b2b7d5fdb
jinhyuek <jjh943202@naver.com>
2024-04-08 01:18:00 +0900

    페이징기능 구현 완료, 페이징기능 컴포넌트화

---
+++ src/app/(afterLogin)/posts/_component/Pagination.tsx
@@ -0,0 +1,103 @@
+type Props = {
+    page: number;
+    setPage: (page: number) => void;
+    startPage: number;
+    setStartPage: (startPage: number) => void;
+    totalPages: number; //전체 페이지 수
+};
+
+function Pagination({ page, setPage, startPage, setStartPage, totalPages }: Props) {
+    const handlePageChange = (pageNumber: number) => {
+        setPage(pageNumber);
+    };
+    const handleSinglePrevPage = () => {
+        if (page % 6 === 2 && page !== 2) setStartPage(startPage - 6);
+        handlePageChange(page - 1);
+    };
+    const handleSingleNextPage = () => {
+        if (page % 6 === 1 && page !== totalPages - 1 && page !== 1) setStartPage(startPage + 6);
+        handlePageChange(page + 1);
+    };
+
+    //...버튼 클릭시 이전 페이지 6개 보여주기
+    const handlePrevGroupPage = () => {
+        setStartPage(startPage - 6);
+        setPage(startPage - 6);
+    };
+
+    //...버튼 클릭시 다음 페이지 6개 보여주기
+    const handleNextGroupPage = () => {
+        setStartPage(startPage + 6);
+        setPage(startPage + 6);
+    };
+
+    return (
+        <div className="mt-4 flex flex-row gap-2">
+            {/* 이전페이지 */}
+            <button
+                onClick={handleSinglePrevPage}
+                className="mr-2 flex flex-row items-center rounded-md border border-neutral-300 px-2 text-sm hover:border-neutral-500 disabled:opacity-30"
+                disabled={page === 1}
+            >
+                <span className="text-xs text-neutral-800">이전 페이지</span>
+            </button>
+
+            {/* 첫페이지 */}
+            <button
+                onClick={() => {
+                    handlePageChange(1);
+                    setStartPage(2);
+                }}
+                className={`h-7 w-7 rounded-md border border-neutral-300 text-sm font-light hover:border-neutral-600 ${page === 1 ? ' bg-primary bg-opacity-80 text-white' : ''}`}
+            >
+                1
+            </button>
+
+            {/* 가려진 이전페이지 */}
+            <button
+                onClick={handlePrevGroupPage}
+                className={`h-7 w-7 rounded-md border border-neutral-300 font-light hover:border-neutral-600 ${page === 1 ? ' bg-primary bg-opacity-80 text-white' : ''} ${page < 8 ? 'hidden' : ''}`}
+            >
+                {`...`}
+            </button>
+
+            {Array.from({ length: Math.min(6, totalPages - startPage) }, (_, i) => startPage + i).map((pageNum) => (
+                <button
+                    key={pageNum}
+                    onClick={() => handlePageChange(pageNum)}
+                    className={`h-7 w-7 rounded-md border border-neutral-300 text-sm font-light hover:border-neutral-600 ${page === pageNum ? ' bg-primary bg-opacity-80 text-white' : ''}`}
+                >
+                    {pageNum}
+                </button>
+            ))}
+
+            {/* 가려진 다음페이지 */}
+            <button
+                onClick={handleNextGroupPage}
+                className={`h-7 w-7 rounded-md border border-neutral-300 font-light hover:border-neutral-600  ${page === totalPages ? ' bg-primary bg-opacity-80 text-white' : ''} ${totalPages - startPage < 6 ? 'hidden' : ''}`}
+            >{`...`}</button>
+
+            {/* 마지막페이지  */}
+            <button
+                onClick={() => {
+                    handlePageChange(totalPages);
+                    setStartPage(totalPages - ((totalPages - 2) % 6));
+                }}
+                className={`h-7 w-7 rounded-md border border-neutral-300 text-sm font-light hover:border-neutral-600 ${page === totalPages ? ' bg-primary bg-opacity-80 text-white' : ''} ${totalPages === 1 ? 'hidden' : ''}`}
+            >
+                {totalPages || 2}
+            </button>
+
+            {/* 다음페이지 */}
+            <button
+                onClick={handleSingleNextPage}
+                className="ml-2 flex flex-row items-center rounded-md border border-neutral-300 px-2 text-sm hover:border-neutral-500 disabled:opacity-30"
+                disabled={totalPages === page}
+            >
+                <span className="text-xs text-neutral-800">다음 페이지</span>
+            </button>
+        </div>
+    );
+}
+
+export default Pagination;
--- src/app/(afterLogin)/posts/mentor/page.tsx
+++ src/app/(afterLogin)/posts/mentor/page.tsx
@@ -1,16 +1,12 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import FaChevronRight from '../../_component/icon/chevronRight';
-import FaChevronLeft from '../../_component/icon/chevronLeft';
 import MentoPostCard from '../../_component/MentoPostCard';
 import Link from 'next/link';
-import Pagination from 'react-js-pagination';
-import axios from 'axios';
-import Axios from '@/app/util/axiosInstance';
 import { useQuery } from '@tanstack/react-query';
 import { MentoContent, MentoPosts, fetchMentorPosts } from '../_lib/posts';
 import Loading from '@/app/_component/Loading';
+import Pagination from '../_component/Pagination';
 
 // const mentorPosts = Array.from({ length: 200 }, (_, i) => `title ${i + 1}`);
 
@@ -21,25 +17,13 @@ export default function MentorsDetail() {
         queryKey: ['mentorPosts', page],
         queryFn: () => fetchMentorPosts(page),
     }); //이후 캐시처리 필요!
-    const handlePageChange = (pageNumber: number) => {
-        setPage(pageNumber);
-    };
+
     useEffect(() => {
         if (data) {
             console.log(data);
         }
     }, [data]);
     const [startPage, setStartPage] = useState<number>(2); //시작 페이지
-    const handlePrevClick = () => {
-        setStartPage(startPage - 6);
-        setPage(startPage - 6);
-    };
-
-    // 다음 버튼을 클릭했을 때의 처리
-    const handleNextClick = () => {
-        setStartPage(startPage + 6);
-        setPage(startPage + 6);
-    };
 
     if (isLoading) return <Loading />; //로딩중 div 반환
     if (isError) return <span className="font-bold text-red-500">에러 발생</span>; //에러 발생 div 반환
@@ -55,76 +39,13 @@ export default function MentorsDetail() {
             </div>
 
             {/* 페이지네이션 */}
-            <div className="mt-4 flex flex-row gap-2">
-                {/* 이전페이지 */}
-                <button
-                    onClick={() => {
-                        handlePageChange(page - 1);
-                    }}
-                    className="mr-2 flex flex-row items-center rounded-md border border-neutral-300 px-2 text-sm hover:border-neutral-500 disabled:opacity-30"
-                    disabled={page === 1}
-                >
-                    <span className="text-xs text-neutral-800">이전 페이지</span>
-                </button>
-
-                {/* 첫페이지 */}
-                <button
-                    onClick={() => {
-                        handlePageChange(1);
-                        setStartPage(2);
-                    }}
-                    className={`h-7 w-7 rounded-md border border-neutral-300 text-sm font-light hover:border-neutral-600 ${page === 1 ? ' bg-primary bg-opacity-80 text-white' : ''}`}
-                >
-                    1
-                </button>
-
-                {/* 가려진 이전페이지 */}
-                <button
-                    onClick={handlePrevClick}
-                    className={`h-7 w-7 rounded-md border border-neutral-300 font-light hover:border-neutral-600 ${page === 1 ? ' bg-primary bg-opacity-80 text-white' : ''} ${page < 8 ? 'hidden' : ''}`}
-                >
-                    {`...`}
-                </button>
-
-                {Array.from(
-                    { length: Math.min(6, data!.pageInfo.totalPages - startPage) },
-                    (_, i) => startPage + i,
-                ).map((pageNum) => (
-                    <button
-                        key={pageNum}
-                        onClick={() => handlePageChange(pageNum)}
-                        className={`h-7 w-7 rounded-md border border-neutral-300 text-sm font-light hover:border-neutral-600 ${page === pageNum ? ' bg-primary bg-opacity-80 text-white' : ''}`}
-                    >
-                        {pageNum}
-                    </button>
-                ))}
-
-                {/* 가려진 다음페이지 */}
-                <button
-                    onClick={handleNextClick}
-                    className={`h-7 w-7 rounded-md border border-neutral-300 font-light hover:border-neutral-600  ${page === data?.pageInfo.totalPages ? ' bg-primary bg-opacity-80 text-white' : ''} ${data!.pageInfo.totalPages - startPage < 6 ? 'hidden' : ''}`}
-                >{`...`}</button>
-
-                {/* 마지막페이지  */}
-                <button
-                    onClick={() => {
-                        handlePageChange(data!.pageInfo.totalPages);
-                        setStartPage(data!.pageInfo.totalPages - ((data!.pageInfo.totalPages - 2) % 6));
-                    }}
-                    className={`h-7 w-7 rounded-md border border-neutral-300 text-sm font-light hover:border-neutral-600 ${page === data?.pageInfo.totalPages ? ' bg-primary bg-opacity-80 text-white' : ''} ${data?.pageInfo.totalPages === 1 ? 'hidden' : ''}`}
-                >
-                    {data?.pageInfo.totalPages || 2}
-                </button>
-
-                {/* 다음페이지 */}
-                <button
-                    onClick={() => handlePageChange(page + 1)}
-                    className="ml-2 flex flex-row items-center rounded-md border border-neutral-300 px-2 text-sm hover:border-neutral-500 disabled:opacity-30"
-                    disabled={data?.pageInfo.totalPages === page}
-                >
-                    <span className="text-xs text-neutral-800">다음 페이지</span>
-                </button>
-            </div>
+            <Pagination
+                page={page}
+                setPage={setPage}
+                startPage={startPage}
+                setStartPage={setStartPage}
+                totalPages={data!.pageInfo.totalPages}
+            />
         </div>
     );
 }
