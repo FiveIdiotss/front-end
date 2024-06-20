949e97bc1a8294d1f29280b4126b2c3d946e7863
jinhyuek <jjh943202@naver.com>
2024-06-21 05:21:04 +0900

    Feat: 채팅방 스크롤 이벤트 감지 및 조작 기능 최하단 옵저버, 3번째 메시지에 옵저버 추가로 특정 스크롤에 도달시 이벤트 실행

--- src/app/(afterLogin)/(nonHeader)/chat/[slug]/_component/ChatRoomContent.tsx
+++ src/app/(afterLogin)/(nonHeader)/chat/[slug]/_component/ChatRoomContent.tsx
@@ -4,24 +4,14 @@ import { useChatStore } from '@/app/(afterLogin)/_store/chatStore';
 import Image from 'next/image';
 import { v4 as uuidv4 } from 'uuid';
 
-import {
-    DefaultError,
-    InfiniteData,
-    useInfiniteQuery,
-    useMutation,
-    useQuery,
-    useQueryClient,
-} from '@tanstack/react-query';
+import { DefaultError, InfiniteData, useInfiniteQuery, useQuery, useQueryClient } from '@tanstack/react-query';
 import { Message, getChatContentList } from '../../_lib/chatContentList';
 import { useInView } from 'react-intersection-observer';
 import { MemberDto } from '@/auth';
-import { set } from 'lodash';
 import Loading from '@/app/_component/Loading';
 import DotLoadingIcon from '@/app/(afterLogin)/_component/icon/DotLoadingIcon';
-import FileIcon from '@/app/(afterLogin)/_component/icon/FileIcon';
-import FilterIcon from '@/app/(afterLogin)/_component/icon/FilterIcon';
-import Link from 'next/link';
 import ChatRoomContentPreload from './ChatRoomContentPreload';
+import ArrowDropIcon from '@/app/(afterLogin)/_component/icon/ArrowDropIcon';
 
 function dateTransform(date: string) {
     try {
@@ -40,32 +30,36 @@ function dateTransform(date: string) {
 
 function ChatRoomContent({ roomId, memberDto }: { roomId: number; memberDto?: MemberDto }) {
     const queryClient = useQueryClient();
-    const [contentScrollHeight, setContentScrollHeight] = useState({
-        previous: 0,
-        current: 0,
-    });
+    const scrollContainerRef = useRef<HTMLDivElement>(null); // 스크롤 컨테이너 ref
+    const [isNewMessage, setIsNewMessage] = useState<boolean>(false); //새로운 메시지 팝업 여부
+    const [isMessage, setIsMovingBar] = useState<boolean>(false); //스크롤바 이동 여부
     const {
         receiverImageUrl,
         receiverId,
         receiverName,
         chatList,
-        // chatRoomId,
         isSending,
         isReceiving,
         setIsReceiving,
-
         setChatList,
         setChatReset,
         setIsSending,
     } = useChatStore();
+
     const senderId = memberDto?.id; //세션정보에서 senderId 추출
+
+    const { ref, inView } = useInView({
+        threshold: 0,
+        delay: 300,
+    }); //무한스크롤 경계선 감지 기능, inView가 true가 되면 fetchNextPage실행
+    const { ref: isMessageInViewRef, inView: inViewMessage } = useInView({}); //새로운 메시지 팝업 감지
+    const { ref: isBottomRef, inView: inViewBottom } = useInView({});
+
     const {
-        data: initialChatList,
-        isFetching,
+        data: initialChatList, //서버에서 불러온 데이터 기반 채팅리스트 초기화
         isPending,
         isError,
         hasNextPage,
-
         fetchNextPage,
     } = useInfiniteQuery<Message[], DefaultError, InfiniteData<Message[]>, [string, number, string], number>({
         queryKey: ['chat', roomId, 'massage'],
@@ -92,52 +86,36 @@ function ChatRoomContent({ roomId, memberDto }: { roomId: number; memberDto?: Me
             setChatList(initialChatList.pages[initialChatList.pages.length - 1]);
             console.log('initialChatList', initialChatList);
         }
-    }, [initialChatList]); //이전 채팅리스트 초기화()
-
-    const scrollContainerRef = useRef<HTMLDivElement>(null); // 스크롤 컨테이너 ref
-    const { ref, inView } = useInView({
-        threshold: 0,
-        delay: 300,
-    }); //무한스크롤 경계선 감지 기능, inView가 true가 되면 fetchNextPage실행
+    }, [initialChatList]); //서버에서 불러온 데이터 기반 이전 채팅리스트 초기화
 
     useLayoutEffect(() => {
         const scrollContainer = scrollContainerRef.current;
 
-        if (scrollContainerRef.current && !pageRendered && chatList.length > 0) {
-            scrollContainerRef.current.scrollTop = scrollContainerRef.current.scrollHeight;
+        if (scrollContainer && !pageRendered && chatList.length > 0) {
+            scrollContainer.scrollTop = scrollContainer.scrollHeight;
             setPageRendered(true); //채팅리스트가 처음 사용자에게  보여질때 한번만 맨아래로 스크롤
-            setContentScrollHeight({
-                previous: scrollContainerRef.current.scrollHeight,
-                current: scrollContainerRef.current.scrollHeight,
-            }); //컨텐트의 스크롤 높이를 저장
-
             return;
-        }
+        } //첫화면에서 맨아래로 스크롤
+
         if (scrollContainer && isReceiving) {
-            const { scrollTop, scrollHeight, clientHeight } = scrollContainer;
-            const contentHeight = scrollContainer.scrollHeight - contentScrollHeight.previous;
-            // const isScrolledToBottom = Math.abs(scrollHeight - scrollTop - clientHeight - contentHeight);
-            // console.log('isScrolledToBottom ', isScrolledToBottom);
-            console.log('contentHeight', contentHeight);
-            // if (isScrolledToBottom < contentHeight + 2) {
-            //     scrollContainer.scrollTop = scrollContainer.scrollHeight;
-            // }
+            if (inViewMessage) {
+                scrollContainer.scrollTop = scrollContainer.scrollHeight;
+                return;
+            } else {
+                setIsNewMessage(true);
+            }
             setIsReceiving(false);
-            // setContentScrollHeight({s
-            //     previous: contentScrollHeight.current,
-            //     current: scrollContainer.scrollHeight,
-            // });
-        }
-    }, [chatList]); //채팅리스트가 처음 사용자에게 보여질때 맨아래로 스크롤
+        } //최근 3번째 채팅 메시지가 뷰에서 사라질때&&새로운 메시지가 도착했을때 새로운 메시지 팝업 여부
+
+        if (scrollContainer && isSending) {
+            scrollContainer.scrollTop = scrollContainer.scrollHeight;
+            setIsSending(false);
+            return;
+        } //본인이 메시지 전송시  스크롤 맨아래로 이동
+    }, [chatList]); //메시지 리스트 변경될때
 
     useEffect(() => {
         const scrollContainer = scrollContainerRef.current;
-
-        if (scrollContainerRef.current && isSending) {
-            scrollContainerRef.current.scrollTop = scrollContainerRef.current.scrollHeight;
-            setIsSending(false);
-            return;
-        }
 
         if (inView && hasNextPage && scrollContainer) {
             const previousScrollHeight = scrollContainer.scrollHeight;
@@ -156,89 +134,119 @@ function ChatRoomContent({ roomId, memberDto }: { roomId: number; memberDto?: Me
                 });
             });
         }
-    }, [inView, isSending]); //무한스크롤
-
-    // useEffect(() => {
-    //     if (isReceiving) {
-    //         const scrollContainer = scrollContainerRef.current;
-    //         if (scrollContainer) {
-    //             const { scrollTop, scrollHeight, clientHeight } = scrollContainer;
-    //             const isScrolledToBottom = Math.abs(scrollHeight - scrollTop - clientHeight);
-    //             console.log('받는메시시 스크롤 높이', isScrolledToBottom);
-    //             if (isScrolledToBottom < 100) {
-    //                 scrollContainer.scrollTop = scrollContainer.scrollHeight;
-    //             }
-    //             setIsReceiving(false);
-    //         }
-    //     }
-    // }, [isReceiving]); //채팅방에 새로운 메시지가 도착했을때 스크롤을 맨아래로
+    }, [inView]); //div 최상단 도달시 데이터 추가
+    useEffect(() => {
+        if (inViewBottom) {
+            setIsNewMessage(false);
+        }
+    }, [inViewBottom]); //div 최하단 도달시 새로운 메시지 팝업 숨김
 
     useEffect(() => {
         queryClient.invalidateQueries({
             queryKey: ['chat', 'List'],
             exact: true,
-        });
+        }); //채팅방  들어올때 채팅리스트 새로고침
+
         return () => {
             queryClient.removeQueries({
                 queryKey: ['chat', roomId, 'massage'],
                 exact: true,
             });
             setChatReset();
-        };
-    }, []); //채팅방 나갈때 초기화
+        }; //채팅방 나갈때 초기화
+    }, []);
+
+    const handleScrollBottom = () => {
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollTop = scrollContainerRef.current.scrollHeight;
+        }
+    }; //스크롤 div 최하단 이동
+
     if (isPending) {
         return <Loading />;
     }
     return (
-        <div className="chatScroll  flex w-full  flex-grow flex-col overflow-y-scroll " ref={scrollContainerRef}>
+        <div className="chatScroll    flex  w-full flex-grow flex-col overflow-y-scroll " ref={scrollContainerRef}>
+            {/* 무한스크롤 옵저버 */}
             <div ref={ref} className={`flex w-full justify-center ${!hasNextPage ? 'hidden' : ''}  `}>
                 <div className="h-10 w-10">
                     <DotLoadingIcon />
                 </div>
             </div>
 
+            {/* 메시지카드를 렌더링하는 부분 */}
             {[...chatList].reverse().map((chat, index) => {
                 const isSender = chat.senderId === senderId;
 
                 return (
-                    <div
-                        key={uuidv4()}
-                        className={`flex ${isSender ? 'flex-row-reverse' : 'flex-row'} gap-1 px-4 py-3`}
-                    >
-                        {!isSender && (
-                            <div>
-                                <Image
-                                    src={receiverImageUrl}
-                                    alt="avatar"
-                                    className="rounded-2xl object-cover"
-                                    width={40}
-                                    height={40}
-                                />
-                            </div>
-                        )}
-                        <div className={`flex flex-col ${isSender ? 'items-end' : 'items-start'} justify-center gap-1`}>
-                            {!isSender && <span className="font-sans text-xs font-medium">{receiverName}</span>}
-                            <div className="flex flex-row gap-1">
-                                <div
-                                    className={`flex flex-col ${isSender ? 'items-end ' : 'hidden items-start'} justify-end`}
-                                >
-                                    <span className="font-sans text-xs font-normal">
-                                        {dateTransform(chat.localDateTime)}
-                                    </span>
+                    <>
+                        {/*특정 메시지 사라짐 감지 옵저버*/}
+                        <div
+                            ref={chatList.length - index === 3 ? isMessageInViewRef : null}
+                            className={`${chatList.length - index === 3 ? '' : 'hidden'}`}
+                        />
+                        <div
+                            key={uuidv4()}
+                            className={`flex ${isSender ? 'flex-row-reverse' : 'flex-row'} gap-1 px-4 py-3`}
+                        >
+                            {!isSender && (
+                                <div>
+                                    <Image
+                                        src={receiverImageUrl}
+                                        alt="avatar"
+                                        className="rounded-2xl object-cover"
+                                        width={40}
+                                        height={40}
+                                    />
                                 </div>
-                                <ChatRoomContentPreload isSender={isSender} chat={chat} />
-                                <div
-                                    className={`flex flex-col ${isSender ? 'hidden items-end' : 'items-start'} justify-end`}
-                                >
-                                    <span className="font-sans text-xs font-normal">
-                                        {dateTransform(chat.localDateTime)}
-                                    </span>
+                            )}
+                            <div
+                                className={`flex flex-col ${isSender ? 'items-end' : 'items-start'} justify-center gap-1`}
+                            >
+                                {!isSender && <span className="font-sans text-xs font-medium">{receiverName}</span>}
+                                <div className="flex flex-row gap-1">
+                                    <div
+                                        className={`flex flex-col ${isSender ? 'items-end ' : 'hidden items-start'} justify-end`}
+                                    >
+                                        <span className="font-sans text-xs font-normal">
+                                            {dateTransform(chat.localDateTime)}
+                                        </span>
+                                    </div>
+                                    <ChatRoomContentPreload isSender={isSender} chat={chat} />
+                                    <div
+                                        className={`flex flex-col ${isSender ? 'hidden items-end' : 'items-start'} justify-end`}
+                                    >
+                                        <span className="font-sans text-xs font-normal">
+                                            {dateTransform(chat.localDateTime)}
+                                        </span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </>
                 );
             })}
+
+            {/* 신규메시지 여부와 스크롤을 하단으로 이동하는 버튼 */}
+            <div
+                className={`bg-slate-black sticky bottom-2 flex h-10 w-full items-center px-4 ${inViewMessage ? 'hidden' : 'justify-end'}`}
+            >
+                <div className={`flex flex-1 justify-center first-line:ml-10 ${isNewMessage ? '' : 'hidden'}`}>
+                    <button
+                        onClick={handleScrollBottom}
+                        className="flex flex-row items-center gap-1 rounded-md border border-gray-300 bg-white bg-opacity-80 px-5 py-2 shadow-md"
+                    >
+                        <span className="text-sm font-medium text-green-600">*미확인 메세지</span>
+                    </button>
+                </div>
+                <button
+                    onClick={handleScrollBottom}
+                    className="ml-2 flex h-10 w-10 items-center justify-center rounded-full border border-gray-300 bg-white bg-opacity-80 shadow-md"
+                >
+                    <ArrowDropIcon isOpen={false} className="h-7 w-7 text-neutral-500" />
+                </button>
+            </div>
+            <div ref={isBottomRef} className="w-full" />
         </div>
     );
 }
