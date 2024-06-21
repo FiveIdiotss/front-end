e66d1ed668bd695efca2238629369b82a4d30c86
jinhyuek <jjh943202@naver.com>
2024-06-22 06:36:46 +0900

    Refactor:채팅방 zustand 정보변경 isLoginMentor, startTime, consultTime, loginId 추가

--- src/app/(afterLogin)/_store/chatStore.ts
+++ src/app/(afterLogin)/_store/chatStore.ts
@@ -1,19 +1,26 @@
 import { create } from 'zustand';
 // import { ChatUsers } from '../(nonHeader)/chat/_lib/chatList';
-import { Message } from '../(nonHeader)/chat/_lib/chatContentList';
+import { Message } from '../(nonHeader)/chat/[slug]/_lib/chatContentList';
 
-type ChatStore = {
-    chatRoomId: number;
-    receiverId: number | undefined;
-    receiverName: string;
-    receiverImageUrl: string;
-    boardTitle: string;
+type ChatStoreType = {
+    //---------채팅방 정보----------
+    chatRoomId: number; //채팅방 아이디
+
+    receiverId: number | undefined; //받는 사람 아이디
+    receiverName: string; //받는 사람 이름
+    receiverImageUrl: string; //받는 사람 이미지
+    boardTitle: string; //게시글 제목
+    loginId: number | undefined; //보내는 사람 아이디
+    isLoginMentor: boolean; //보내는 사람 멘토 여부{true:멘토, false:멘티}
+    startTime: string; //매칭 시작시간
+    consultTime: number; //상담시간
     latestMessageDTO: {
         content: string;
         hasImage: boolean;
         localDateTime: string;
-    }; //채팅방 정보
+    }; //마지막 메시지에대한 정보
 
+    //---------채팅리스트 정보----------
     chatList: Message[];
     isSending: boolean;
     isReceiving: boolean;
@@ -27,22 +34,35 @@ type ChatStore = {
         boardTitle,
         receiverImageUrl,
         receiverName,
+        loginId,
+        isLoginMentor,
+        startTime,
+        consultTime,
     }: {
         receiverId: number;
         boardTitle: string;
         receiverImageUrl: string;
         receiverName: string;
-    }) => void;
-    setChat: (chatList: Message) => void;
-    setChatList: (chatList: Message[]) => void;
-    setChatReset: () => void;
+        loginId: number;
+        isLoginMentor: boolean;
+        startTime: string;
+        consultTime: number;
+    }) => void; //채팅방 정보 설정
+
+    setChat: (chatList: Message) => void; //로컬에서 새로운 채팅리스트 추가
+    setChatList: (chatList: Message[]) => void; //서버에서 새로운 채팅리스트 추가
+    setChatReset: () => void; //채팅리스트 초기화
 };
-export const useChatStore = create<ChatStore>((set) => ({
+export const useChatStore = create<ChatStoreType>((set) => ({
     chatRoomId: -1,
     receiverId: undefined,
     receiverName: '',
     receiverImageUrl: '',
     boardTitle: '',
+    startTime: '',
+    consultTime: 0,
+    loginId: undefined,
+    isLoginMentor: false,
     latestMessageDTO: {
         content: '',
         hasImage: false,
@@ -52,20 +72,28 @@ export const useChatStore = create<ChatStore>((set) => ({
     chatList: [],
     isSending: false,
     isReceiving: false,
+    image: '', //채팅리스트 정보
 
-    image: '',
     setUserInformation: ({
         receiverId,
         boardTitle,
         receiverImageUrl,
         receiverName,
+        loginId,
+        isLoginMentor,
+        startTime,
+        consultTime,
     }: {
         receiverId: number;
         boardTitle: string;
         receiverImageUrl: string;
         receiverName: string;
+        loginId: number;
+        isLoginMentor: boolean;
+        startTime: string;
+        consultTime: number;
     }) => {
-        set({ receiverId, boardTitle, receiverImageUrl, receiverName });
+        set({ receiverId, boardTitle, receiverImageUrl, receiverName, loginId, isLoginMentor, startTime, consultTime });
     }, //채팅방 정보 설정
 
     setChat: (newChatList: Message) => {
@@ -78,11 +106,11 @@ export const useChatStore = create<ChatStore>((set) => ({
     }, //서버에서 새로운 채팅리스트 추가
     setIsSending: (isSending: boolean) => {
         set({ isSending });
-    },
+    }, //보내는중인지 여부
     setIsReceiving: (isReceiving: boolean) => {
         set({ isReceiving });
-    },
+    }, //받는중인지 여부
     setChatReset: () => {
         set({ chatList: [] });
-    },
+    }, //채팅리스트 초기화
 }));
