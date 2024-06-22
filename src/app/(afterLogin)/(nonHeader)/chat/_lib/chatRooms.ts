964d193f00401879d2560235fc365fcd6c12430e
jinhyuek <jjh943202@naver.com>
2024-06-22 15:27:50 +0900

    Refactor: 타입명 변경및 타입 추가 Charoom->ChatRoomType mentorId, startTime,consultTime 타입 추가

--- src/app/(afterLogin)/(nonHeader)/chat/_lib/chatRooms.ts
+++ src/app/(afterLogin)/(nonHeader)/chat/_lib/chatRooms.ts
@@ -1,10 +1,9 @@
 import { ErrorResponse } from '@/app/Models/AxiosResponse';
 import Axios from '@/app/util/axiosInstance';
-import { faker } from '@faker-js/faker';
 import { AxiosError } from 'axios';
 import { getSession } from 'next-auth/react';
 
-export type ChatRoom = {
+export type ChatRoomType = {
     chatRoomId: number; //채팅방 아이디
     receiverId: number; //상대방 아이디
     receiverName: string; //상대방 이름
@@ -17,12 +16,14 @@ export type ChatRoom = {
     boardTitle: string; //매칭및 상담신청한 보드타이틀
     boardId: number; //매칭및 상담신청한 보드아이디
     matchingId: number; //현제 매칭 정보 조회용
+    mentorId: number; //멘토 아이디
+    startTime: string; //매칭 시작시간
+    consultTime: number; //상담시간
 }; // 채팅방 리스트
 
-const avatar = faker.image.avatar();
 const url = process.env.NEXT_PUBLIC_API_URL;
 
-export async function getChatRooms(): Promise<ChatRoom[]> {
+export async function getChatRooms(): Promise<ChatRoomType[]> {
     const session = await getSession();
     const memberId = session?.user?.memberDTO?.id;
 
