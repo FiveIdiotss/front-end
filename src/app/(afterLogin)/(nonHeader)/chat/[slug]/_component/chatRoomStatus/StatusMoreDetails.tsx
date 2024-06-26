ebeae2a42d660d9dc75f7be757aea5481d6df6b2
jinhyuek <jjh943202@naver.com>
2024-06-26 20:45:15 +0900

    Feat: 날짜 변환 커스텀 훅 구현 날짜 변환 커스텀 훅으로 전환

--- src/app/(afterLogin)/(nonHeader)/chat/[slug]/_component/chatRoomStatus/StatusMoreDetails.tsx
+++ src/app/(afterLogin)/(nonHeader)/chat/[slug]/_component/chatRoomStatus/StatusMoreDetails.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useFormattedTime } from '@/app/(afterLogin)/(nonHeader)/_hooks/useFormattedConsultTimes';
 import { useChatStore } from '@/app/(afterLogin)/_store/chatStore';
 import { useCountdown } from '@/app/util/CountDownHook';
 import React from 'react';
@@ -9,18 +10,7 @@ function StatusMoreDetails() {
     const mentorName = isLoginMentor ? loginName : receiverName;
     const menteeName = isLoginMentor ? receiverName : loginName;
 
-    // startTime을 기준으로 endTime 계산
-    const dateTimeString = `${date}T${startTime}`; //시작시간
-
-    const startTimeDate = new Date(dateTimeString);
-
-    const endTimeDate = new Date(dateTimeString);
-    endTimeDate.setMinutes(startTimeDate.getMinutes() + consultTime); // 상담 시간 + 분 추가
-
-    const { hours = 0, minutes = 0, seconds = 0 } = useCountdown(startTimeDate);
-
-    const formattedStartTime = `${String(startTimeDate.getHours()).padStart(2, '0')} : ${String(startTimeDate.getMinutes()).padStart(2, '0')}`;
-    const formattedEndTime = `${String(endTimeDate.getHours()).padStart(2, '0')} : ${String(endTimeDate.getMinutes()).padStart(2, '0')}`;
+    const { formattedStartTime, formattedEndTime, formattedDate } = useFormattedTime();
 
     return (
         <div className="mt-2 flex w-full flex-col items-center gap-6 rounded-lg p-6 text-base text-neutral-600 ">
@@ -36,9 +26,10 @@ function StatusMoreDetails() {
                 </span>
             </div>
 
-            <div className="flex  w-full flex-col items-center rounded-lg bg-blue-50 p-4">
+            <div className="flex  w-full flex-col  gap-2 rounded-lg bg-blue-50 p-4">
+                <span className="mb-3 font-bold ">{formattedDate}</span>
                 <span className="font-bold text-blue-700">시작시간: {formattedStartTime}</span>
-                <span className="text-neutral-500">-</span>
+
                 <span className="font-bold text-red-700">종료시간: {formattedEndTime}</span>
             </div>
             {/* <span>{`남은시간:${hours}:${minutes}:${seconds}`}</span> */}
