3d224b27b76f190048f7bb0542170522d3ea47f9
jinhyuek <jjh943202@naver.com>
2024-08-17 20:27:47 +0900

    Refacotr: css 수정

--- src/app/(afterLogin)/(nonHeader)/chat/[slug]/_component/chatRoomStatus/ChatRoomStatus.tsx
+++ src/app/(afterLogin)/(nonHeader)/chat/[slug]/_component/chatRoomStatus/ChatRoomStatus.tsx
@@ -8,7 +8,7 @@ import StatusMoreDetails from './StatusMoreDetails';
 
 function ChatRoomStatus() {
     return (
-        <div className="scroll chatScroll flex w-1/4 min-w-[260px] flex-col overflow-y-auto border-l border-neutral-300   px-3">
+        <div className="scroll chatScroll hidden w-1/4 min-w-[260px] flex-col overflow-y-auto border-l border-neutral-300   px-3 mobile:flex">
             <div className="bg-yellow flex  flex-col border-b border-neutral-300 px-4 py-4">
                 <StatusReceiverInfo />
             </div>
