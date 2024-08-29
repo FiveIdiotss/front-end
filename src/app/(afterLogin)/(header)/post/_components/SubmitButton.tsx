e51ba4e80022ea92e3db4dabf9a3a73670fd40e9
jinhyuek <jjh943202@naver.com>
2024-08-30 01:19:14 +0900

    Refactor: type으로 submit,button을  받는 props추가 및 button에 적용

--- src/app/(afterLogin)/(header)/post/_components/SubmitButton.tsx
+++ src/app/(afterLogin)/(header)/post/_components/SubmitButton.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 
 type SubmitButtonProps = {
     cancelUrl: string;
-    onSubmit: () => void;
+    onSubmit?: () => void;
     isLoading: boolean;
+    type?: 'submit' | 'button';
 };
-function SubmitButton({ cancelUrl, onSubmit, isLoading }: SubmitButtonProps) {
+function SubmitButton({ cancelUrl, onSubmit, isLoading, type = 'button' }: SubmitButtonProps) {
     return (
-        <div className="shadow-sm-top fixed bottom-0 left-0  flex w-full flex-row justify-center gap-4 bg-gray-50 py-2 ">
+        <div className="fixed bottom-0 left-0 flex  w-full flex-row justify-center gap-4 bg-gray-50 py-2 shadow-sm-top ">
             <div className="flex w-full max-w-[800px] flex-row justify-end gap-5 px-7">
-                <button className="h-11 w-20 rounded-md border border-neutral-300 bg-neutral-100 text-neutral-600 ">
+                <button
+                    type={type}
+                    className="h-11 w-20 rounded-md border border-neutral-300 bg-neutral-100 text-neutral-600 "
+                >
                     취소
                 </button>
                 <button
