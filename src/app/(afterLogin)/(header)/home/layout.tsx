43d33a3697e559ced9b372fffb70490fba1ce1af
jinhyuek <jjh943202@naver.com>
2024-08-30 16:20:00 +0900

    Refactor: 메인 페이지 상단 배너 모바일 반응형 추가

--- src/app/(afterLogin)/(header)/home/layout.tsx
+++ src/app/(afterLogin)/(header)/home/layout.tsx
@@ -4,17 +4,19 @@ import HeaderSearch from '../../_component/layout/HeaderSearch';
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <div className="w-dvh z-[2] flex flex-col items-center justify-center gap-5 bg-opacity-70 bg-gradient-to-r from-white via-secondary to-white py-7 ">
+            <div className="w-dvh z-[2] flex w-full flex-col items-center justify-center gap-5 bg-opacity-70 bg-gradient-to-r from-white via-secondary to-white py-7 ">
                 <div className=" flex flex-row items-center justify-center gap-7 ">
-                    <BookIcon className="h-12 w-12 text-green-600 " />
+                    <BookIcon className="h-auto w-9 text-green-600 mobile:w-12 " />
                     <div className=" flex  flex-col items-start justify-center gap-1">
-                        <span className="text-2xl font-bold text-black">
+                        <span className="text-xl font-bold text-black mobile:text-2xl">
                             <span className="text-green-600">지식</span>을 나누는 새로운 세상
                         </span>
-                        <span className="text-lg font-medium text-black">멘토링으로 성장하는 여정에 참여하세요!</span>
+                        <span className=" font-medium text-black mobile:text-lg">
+                            멘토링으로 성장하는 여정에 참여하세요!
+                        </span>
                     </div>
                 </div>
-                <div className="w-full">
+                <div className="flex w-full flex-row justify-center ">
                     <HeaderSearch />
                 </div>
             </div>
