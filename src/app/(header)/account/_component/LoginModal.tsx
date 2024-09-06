dff60602b7088233edd3588149784a4c20635bf1
jinhyuek <jjh943202@naver.com>
2024-09-07 02:06:20 +0900

    Feat: auth.ts CredentialsSignin객체 이용해 비밀번호,이메일 불일치 여부 핸들링 하도록 추가

--- src/app/(header)/account/_component/LoginModal.tsx
+++ src/app/(header)/account/_component/LoginModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ChangeEventHandler, FormEventHandler, useState } from 'react';
+import { ChangeEventHandler, FormEventHandler, useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import BackButton from './BackButton';
 import { signIn } from 'next-auth/react';
@@ -11,35 +11,25 @@ import usePrevPageStore from '@/app/_store/prevUrlStore';
 export default function LoginModal() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [message, setMessage] = useState('');
-    const router = useRouter();
+    // const [errorMessage, setErrorMessage] = useState(''); //로그인 에러메세지
+
     const searchParams = useSearchParams();
-    const loginRequired = Boolean(searchParams.get('loginRequired'));
+    const errorType = searchParams.get('error'); //로그인시 에러타입(서버에서 받아올 경우 CredentialsSignin)
+    const errorCode = searchParams.get('code'); //로그인시 에러코드(서버에서 받아올 경우 wrong_password, no_user)
+    const loginRequired = Boolean(searchParams.get('loginRequired')); //로그인 필요 여부
+
+    const router = useRouter();
+
     const { prevUrl } = usePrevPageStore(); //이전 URL 저장 및 반환
     const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
         e.preventDefault();
-        setMessage('');
-
-        try {
-            const response = await signIn('credentials', {
-                username: email,
-                password,
-                // redirect: loginRequired,
-                callbackUrl: prevUrl || '/',
-            });
-            if (response?.error === 'CredentialsSignin') {
-                console.log(response);
-                setMessage('아이디및 비밀번호를 확인해주세요.');
-                return;
-            } else if (response?.error === 'Configuration') {
-                console.log(response);
-                setMessage('서버 오류가 발생했습니다. 잠시후 다시 시도해주세요.');
-                return;
-            }
-            console.log(response);
-        } catch (err) {
-            setMessage('로그인 실패');
-        }
+        // setErrorMessage('');
+        await signIn('credentials', {
+            username: email,
+            password,
+            // redirect: loginRequired,
+            callbackUrl: prevUrl || '/',
+        });
     };
     const onChangeEmail: ChangeEventHandler<HTMLInputElement> = (e) => {
         setEmail(e.target.value);
@@ -48,6 +38,51 @@ export default function LoginModal() {
     const onChangePassword: ChangeEventHandler<HTMLInputElement> = (e) => {
         setPassword(e.target.value);
     };
+    const onClose = () => {
+        console.log('에러타입', errorType);
+        console.log('prevUrl', prevUrl);
+        if (Boolean(errorType)) {
+            router.push(prevUrl);
+        } else {
+            router.back();
+        }
+    };
+
+    // useEffect(() => {
+    //     if (errorType) {
+    //         if (errorType === 'CredentialsSignin') {
+    //             if (errorCode === 'wrong_password') {
+    //                 setErrorMessage('비밀번호가 일치하지 않습니다.');
+    //             } else if (errorCode === 'no_user') {
+    //                 setErrorMessage('해당 이메일이 존재하지 않습니다.');
+    //             } else {
+    //                 setErrorMessage('에러가 발생했습니다. 잠시 후 다시 시도해주세요.');
+    //             }
+    //         } else {
+    //             setErrorMessage('에러가 발생했습니다. 잠시 후 다시 시도해주세요.');
+    //         }
+    //     }
+    // }, [errorType, errorCode]);
+    let errorMessage = '';
+
+    if (errorType) {
+        if (errorType === 'CredentialsSignin') {
+            if (errorCode === 'wrong_password') {
+                errorMessage = '비밀번호가 일치하지 않습니다.';
+            } else if (errorCode === 'no_user') {
+                errorMessage = '해당 이메일이 존재하지 않습니다.';
+            } else {
+                errorMessage = '에러가 발생했습니다. 잠시 후 다시 시도해주세요.';
+            }
+        } else {
+            errorMessage = '에러가 발생했습니다. 잠시 후 다시 시도해주세요.';
+        }
+    }
+    // useEffect(() => {
+    //     if (email !== '' || password !== '') {
+    //         setErrorMessage('');
+    //     }
+    // }, [email, password]);
 
     return (
         // 모달배경
@@ -58,7 +93,7 @@ export default function LoginModal() {
                 <div className="relative flex  h-14 w-full shrink-0 flex-row items-center border-b border-solid px-3">
                     {/* 모달헤더 */}
                     <div className="absolute">
-                        <BackButton />
+                        <BackButton onClose={onClose} />
                     </div>
                 </div>
                 <div className=" flex  w-full flex-grow flex-col items-center  p-6">
@@ -77,7 +112,7 @@ export default function LoginModal() {
                                 value={email}
                                 onChange={onChangeEmail}
                                 type="text"
-                                placeholder="이메일을 입력하세요."
+                                placeholder="abcd@example.com"
                             />
                         </div>
                         <div className="flex w-full flex-col gap-1">
@@ -88,10 +123,13 @@ export default function LoginModal() {
                                 value={password}
                                 onChange={onChangePassword}
                                 type="password"
-                                placeholder="비밀번호를 입력하세요"
+                                placeholder="비밀번호"
                             />
                         </div>
-                        <div className="mt-4 flex w-full flex-col items-center gap-6">
+                        <div className="flex w-full items-center justify-center">
+                            <span className="text-sm text-red-500">{errorMessage}</span>
+                        </div>
+                        <div className=" flex w-full flex-col items-center gap-6">
                             <button
                                 type="submit"
                                 className="flex w-full  flex-row items-center justify-center gap-2 rounded-md border border-solid border-gray-300 bg-primary p-3 text-white"
