4a3b8ee05228cb7e03fed8d2fd4b802368654d0e
jinhyuek <jjh943202@naver.com>
2024-09-07 01:43:39 +0900

    Refactor: persist zutand미들 웨어를 사용해 새로고침해도 prevurl상태가 유지되도록함

--- src/app/_store/prevUrlStore.ts
+++ src/app/_store/prevUrlStore.ts
@@ -1,15 +1,24 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type PrevUrlStore = {
     prevUrl: string; // 현재 URL
     setPrevUrl: (url: string) => void; // 현재 URL 업데이트 함수
 };
 
-const usePrevPageStore = create<PrevUrlStore>((set) => ({
-    prevUrl: '', // 현재 URL
-    setPrevUrl: (url) => {
-        set({ prevUrl: url });
-    },
-}));
+const usePrevPageStore = create<PrevUrlStore>()(
+    persist(
+        (set) => ({
+            prevUrl: '', // 초기 URL 값
+            setPrevUrl: (url) => {
+                set({ prevUrl: url });
+            },
+        }),
+        {
+            name: 'prev-url-store', // localStorage에 저장될 키 이름
+            getStorage: () => sessionStorage, // localStorage를 사용하여 상태를 저장
+        },
+    ),
+);
 
 export default usePrevPageStore;
