587bcbb507687cb6a6228369d0639d2affd74e0a
jinhyuek <jjh943202@naver.com>
2024-09-08 20:08:42 +0900

    Refactor: signUpService.ts -불필요한  import 제거 -불필요한 타입 as 제거 -useQuery 에러타입 추가

--- src/app/(header)/account/_lib/signupService.ts
+++ src/app/(header)/account/_lib/signupService.ts
@@ -3,16 +3,13 @@ import { MajorType, SchoolType, SignupFormType } from '@/app/Models/SignupType';
 import Axios from '@/app/util/axiosInstance';
 import { pushNotification } from '@/app/util/pushNotification';
 import { useMutation, useQuery } from '@tanstack/react-query';
-import axios, { AxiosError } from 'axios';
-const url = process.env.NEXT_PUBLIC_API_URL;
+import { AxiosError } from 'axios';
 
 const postSiginup = async (data: SignupFormType) => {
-    console.log(data);
     delete data.passwordConfirm;
     delete data.majorName;
     delete data.schoolId;
     delete data.validEmail;
-    console.log(data);
 
     const response = await Axios.post(`/api/member/signUp`, data);
 
@@ -21,12 +18,12 @@ const postSiginup = async (data: SignupFormType) => {
 
 export const getSchools = async (): Promise<SchoolType[]> => {
     const response = await Axios.get(`/api/schools`);
-    return response.data.data as SchoolType[];
+    return response.data.data;
 }; //회원가입시 학교목록
 
 export const getMajors = async (schoolName: string = '가천대학교'): Promise<MajorType[]> => {
     const response = await Axios.get(`/api/school/${schoolName}`);
-    return response.data.data as MajorType[];
+    return response.data.data;
 }; //회원가입시 학교별 학과목록
 
 //---------------------------------------------------------------------hooks---------------------------------------------------------------------
@@ -34,31 +31,31 @@ export const getMajors = async (schoolName: string = '가천대학교'): Promise
 export const useSignupMutation = () => {
     const mutation = useMutation({
         mutationFn: postSiginup,
-        onError: (error: AxiosError<ErrorResponse>) => {
-            pushNotification({
-                msg: error.response?.data.message || '알 수 없는 오류가 발생했습니다.',
-                type: 'error',
-                theme: 'dark',
-            });
-        },
+        // onError: (error: AxiosError<ErrorResponse>) => {
+        //     pushNotification({
+        //         msg: error.response?.data.message || '알 수 없는 오류가 발생했습니다.',
+        //         type: 'error',
+        //         theme: 'dark',
+        //     });
+        // },
     });
     return mutation;
 }; //회원가입
 
 export const useSchoolsQuery = () => {
-    const query = useQuery({
+    const query = useQuery<SchoolType[], AxiosError<ErrorResponse>>({
         queryKey: ['schools'],
         queryFn: getSchools,
-        staleTime: 1000 * 60 * 60 * 24,
+        staleTime: 1000 * 60 * 60 * 24, //자주 바뀔것 같지 않아 24시간으로 설정
     });
     return query;
 }; //학교목록
 
 export const useMajorsQuery = ({ schoolName, enabled = false }: { schoolName: string; enabled: boolean }) => {
-    const query = useQuery({
+    const query = useQuery<MajorType[], AxiosError<ErrorResponse>>({
         queryKey: ['majors', schoolName],
         queryFn: () => getMajors(schoolName),
-        staleTime: 1000 * 60 * 60 * 24,
+        staleTime: 1000 * 60 * 60 * 24, //자주 바뀔것 같지 않아 24시간으로 설정
         enabled: enabled,
     });
     return query;
