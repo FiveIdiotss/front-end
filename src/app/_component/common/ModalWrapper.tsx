ae195876aea90573a1f8cc024995f908eb002d85
jinhyuek <jjh943202@naver.com>
2024-09-09 19:49:34 +0900

    Refoctor: 레이아웃 수정

--- src/app/_component/common/ModalWrapper.tsx
+++ src/app/_component/common/ModalWrapper.tsx
@@ -18,21 +18,29 @@ function ModalWrapper({ children, className, title, subTitle, onClose, closeUrl 
     useEffect(() => {
         setIsBrowser(true);
     }, []);
-    useEffect(() => {
-        // 모달이 열릴 때 body의 overflow를 hidden으로 설정
-        document.body.style.overflow = 'hidden';
 
-        // cleanup 함수를 통해 모달이 닫히거나 컴포넌트가 언마운트될 때 overflow를 복원
+    useEffect(() => {
+        // 모달이 열릴 때 body 스크롤을 막음
+        const scrollY = window.scrollY;
+        document.body.style.overflow = 'hidden'; // 스크롤 차단
+        document.body.style.position = 'fixed';
+        document.body.style.top = `-${scrollY}px`;
+        document.body.style.width = '100%';
+
+        // 모달이 닫힐 때 스크롤 복원
         return () => {
             document.body.style.overflow = '';
+            document.body.style.position = '';
+            document.body.style.top = '';
+            window.scrollTo(0, scrollY); // 스크롤 위치 복원
         };
-    }, []); // 빈 배열을 의존성 배열로 제공하여 컴포넌트 마운트 시 한 번만 실행되도록 함
+    }, []); // 모달이 열릴 때만 실행
 
     if (!open || !isBrowser) return null;
 
     return ReactDOM.createPortal(
         <div
-            className="fixed bottom-0 left-0 right-0 top-0 z-[2000] flex h-full  w-screen items-center justify-center bg-modal"
+            className="fixed bottom-0 left-0  right-0 top-0 z-[2000] flex h-dvh  w-dvw items-center justify-center bg-modal"
             onClick={onClose}
         >
             <div
