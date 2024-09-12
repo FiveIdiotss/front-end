631bd8f823eb8c31c8b0cd08ae3cb0e49bb4a09f
jinhyuek <jjh943202@naver.com>
2024-09-13 03:17:11 +0900

    Refactor: 채팅시 프로필 이미 원형으로 랜더링 안되는 현상 수정

--- src/app/(nonHeader)/chat/[slug]/_component/chatRoomContent/ChatItemContainer.tsx
+++ src/app/(nonHeader)/chat/[slug]/_component/chatRoomContent/ChatItemContainer.tsx
@@ -28,20 +28,23 @@ function ChatItemContainer({ chat }: Props) {
     return (
         <div className={`flex ${isUserSentMessage ? 'flex-row-reverse ' : 'flex-row'} w-full gap-1  px-4 py-3`}>
             {!isUserSentMessage && (
-                <div>
+                <div className="relative h-9 w-9 flex-shrink-0">
                     <Image
                         src={receiverImageUrl}
                         alt="avatar"
-                        className="flex-shrink-0 rounded-2xl object-cover"
-                        width={40}
-                        height={40}
+                        className=" rounded-full object-cover"
+                        sizes="60px"
+                        fill
                     />
                 </div>
             )}
             <div
                 className={`flex flex-col ${isUserSentMessage ? 'items-end' : 'items-start'} max-w-96   flex-grow justify-center gap-1 `}
             >
+                {/* 이름 */}
                 {!isUserSentMessage && <span className="font-sans text-xs font-medium">{receiverName}</span>}
+
+                {/* 채팅 내용(보내는 사람의 경우 오른쪽, 받는 사람의 경우 왼쪽) */}
                 <div className={`flex  w-full   flex-grow flex-row gap-1 ${isUserSentMessage ? 'justify-end' : ''} `}>
                     <div
                         className={`flex flex-shrink-0 flex-col ${isUserSentMessage ? 'items-end ' : 'hidden items-start'} justify-end`}
@@ -49,6 +52,7 @@ function ChatItemContainer({ chat }: Props) {
                         <span className="font-sans text-xs font-normal">{dateTransform(chat.localDateTime)}</span>
                     </div>
                     <ChatItem isUserSentMessage={isUserSentMessage} chat={chat} isLoginMentor={isLoginMentor} />
+
                     <div
                         className={`flex flex-shrink-0 flex-col ${isUserSentMessage ? 'hidden ' : 'items-start'} justify-end`}
                     >
