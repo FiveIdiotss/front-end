c1ab7a2bffe826c28fa54bbc6208d39356465502
jinhyuek <jjh943202@naver.com>
2024-11-29 23:34:32 +0900

    Refactor: 멘토링 og 이미지

--- src/app/(header)/posts/mentor/[slug]/opengraph-image.tsx
+++ src/app/(header)/posts/mentor/[slug]/opengraph-image.tsx
@@ -4,8 +4,11 @@ import { createAvatar } from '@dicebear/core';
 import { notionists } from '@dicebear/collection';
 import { htmlToText } from 'html-to-text';
 
-export const runtime = 'edge';
-export const alt = 'About image';
+export const alt = 'About Acme';
+export const size = {
+    width: 1200,
+    height: 630,
+};
 export const contentType = 'image/png';
 
 export default async function Image({ params }: { params: { slug: string } }) {
