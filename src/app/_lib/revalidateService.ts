c7006cf2b5727d07935ffafc2cb5ef87ca4dfa8d
jinhyuek <jjh943202@naver.com>
2025-01-17 17:52:42 +0900

    Test: revalidate 테스트

--- src/app/_lib/revalidateService.ts
+++ src/app/_lib/revalidateService.ts
@@ -1,6 +1,4 @@
 export const getRevalidate = async (tag: string) => {
-    const response = await fetch(
-        `${process.env.NEXT_PUBLIC_HOST || 'https://menteetor.site'}/api/revalidate?tag=${tag}`,
-    );
+    const response = await fetch(`/api/revalidates?tag=${tag}`);
     return response;
 };
